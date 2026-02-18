91ddf8b79687526a5d1db5b86d4c82af9d0e66a2
Sayaka Shen <gaetan.decisy6@hotmail.com>
2026-02-18 16:04:40 +0100

    Edit project struct

--- CrazyLauncher/Project.h
+++ CrazyLauncher/Project.h
@@ -1,14 +1,29 @@
 #pragma once
 #include <QString>
-#include "PathState.h"
+
+enum ProjectType
+{
+	Executable,
+	Script,
+	Unreal,
+	Unity,
+	Blender,
+	Photoshop,
+	Custom,
+};
 
 struct Project
 {
 	QString s_name;
 	QString s_description;
+	ProjectType s_type;
 	QString s_path;
-	QString s_software;
-	PathState s_pathState;
+	bool s_isDirectory;
+	QString s_softwareExe;
 
-	Project(QString& name, QString& descripton, QString& path, QString& software) : s_name(name), s_description(descripton), s_path(path), s_software(software) {};
-};)
+
+	Project(QString& name, QString& descripton, ProjectType type, QString& path, bool isDirectory, QString& softwareExe) 
+		: s_name(name), s_description(descripton), s_type(type), s_path(path), s_isDirectory(isDirectory), s_softwareExe(softwareExe) 
+	{
+	};
+};
